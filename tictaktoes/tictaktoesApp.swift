60c6483475d18eb442fef5169da35547142ed680
Glenn <glenn_liu_chang_hui@s2024.ssts.edu.sg>
2024-11-12 12:24:32 +0800

    Initial Commit

---
+++ tictaktoes/tictaktoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictaktoesApp.swift
+//  tictaktoes
+//
+//  Created by souper shy on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct tictaktoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
