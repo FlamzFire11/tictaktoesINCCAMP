697ac6576fe3d2f2022f8b019b51750a0bbd749a
Dylan_Nguyen_Dang_Khoa <Dylan_Nguyen_Dang_Khoa@s2023.ssts.edu.sg>
2024-11-12 15:09:56 +0800

    A;LKSDJF;ALKWJEDF

--- tictaktoes/TicTacToeGridView.swift
+++ tictaktoes/TicTacToeGridView.swift
@@ -6,17 +6,6 @@
 //
 import SwiftUI
 
-extension Color {
-    init(hex: Int, opacity: Double = 1) {
-        self.init(
-            .sRGB,
-            red: Double((hex >> 16) & 0xff) / 255,
-            green: Double((hex >> 08) & 0xff) / 255,
-            blue: Double((hex >> 00) & 0xff) / 255,
-            opacity: opacity
-        )
-    }
-}
 struct TicTacToeView: View {
     // The game state: Board (3x3 array), current player, winner
     @State private var board: [[String]] = [
